c651edfd69f016478a62499312e7c860c0f3abb5
Colm Doyle <cdoyle@slack-corp.com>
2021-06-15 16:26:50 +0100

    Testing with Jest

--- nodejs/user-interface/modals/task-created.js
+++ nodejs/user-interface/modals/task-created.js
@@ -1,7 +1,7 @@
 const { Modal, Blocks, Elements } = require('slack-block-builder');
 
 module.exports = (taskTitle) => {
-    return Modal({ title: 'Task created', callbackId: 'new-task-modal'})
+    return Modal({ title: 'Task created', callbackId: 'task-created-modal'})
         .blocks(
             Blocks.Section({
                 text: `${taskTitle} created`
--- nodejs/user-interface/modals/task-creation-error.js
+++ nodejs/user-interface/modals/task-creation-error.js
@@ -1,7 +1,7 @@
 const { Modal, Blocks, Elements } = require('slack-block-builder');
 
 module.exports = (taskTitle) => {
-    return Modal({ title: 'Something went wrong', callbackId: 'new-task-modal'})
+    return Modal({ title: 'Something went wrong', callbackId: 'task-creation-error-modal'})
         .blocks(
             Blocks.Section({
                 text: `We couldn't create ${taskTitle}. Sorry!`
