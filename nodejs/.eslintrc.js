f3f793fc475e9642b688fae6617652932f2f1b24
Colm Doyle <colmdoyle@users.noreply.github.com>
2021-06-15 23:34:08 +0100

    Eslint (#21)

---
+++ nodejs/.eslintrc.js
@@ -0,0 +1,16 @@
+module.exports = {
+  env: {
+    browser: true,
+    commonjs: true,
+    es2021: true,
+  },
+  extends: [
+    'airbnb-base',
+    'eslint:recommended',
+  ],
+  parserOptions: {
+    ecmaVersion: 12,
+  },
+  rules: {
+  },
+};
