4a52417a3179b20c33070af577a79a8011170625
Colm Doyle <colmdoyle@users.noreply.github.com>
2021-06-29 22:52:40 +0100

    Add due date to tasks (#29)

--- nodejs/listeners/views/newTaskModal.js
+++ nodejs/listeners/views/newTaskModal.js
@@ -1,3 +1,5 @@
+const { DateTime } = require('luxon');
+
 const { User, Task } = require('../../models');
 const { modals } = require('../../user-interface');
 const { reloadAppHome } = require('../../utilities');
@@ -6,6 +8,35 @@ module.exports = (app) => {
   app.view('new-task-modal', async ({
     ack, view, body, client,
   }) => {
+    const providedValues = view.state.values;
+
+    const taskTitle = providedValues.taskTitle.taskTitle.value;
+
+    const selectedDate = providedValues.taskDueDate.taskDueDate.selected_date;
+
+    const task = {
+      title: taskTitle,
+    };
+
+    if (selectedDate) {
+      const taskDueDate = DateTime.fromISO(providedValues.taskDueDate.taskDueDate.selected_date).endOf('day');
+
+      const diffInDays = taskDueDate.diffNow('days').toObject().days;
+      // Task due date is in the past, so reject
+      if (diffInDays < 0) {
+        await ack(
+          {
+            response_action: 'errors',
+            errors: {
+              taskDueDate: 'Please select a due date in the future',
+            },
+          },
+        );
+        return;
+      }
+      task.dueDate = taskDueDate;
+    }
+
     try {
       const queryResult = await User.findOrCreate({
         where: {
@@ -18,15 +49,13 @@ module.exports = (app) => {
       });
       const user = queryResult[0];
 
-      user.createTask({
-        title: view.state.values.taskTitle.taskTitle.value,
-      });
+      user.createTask(task);
 
       await user.save();
       await ack(
         {
           response_action: 'update',
-          view: modals.taskCreated(view.state.values.taskTitle.taskTitle.value),
+          view: modals.taskCreated(taskTitle),
         },
       );
       await reloadAppHome(client, body.user.id, body.team.id);
@@ -34,7 +63,7 @@ module.exports = (app) => {
       await ack(
         {
           response_action: 'update',
-          view: modals.taskCreationError(view.state.values.taskTitle.taskTitle.value),
+          view: modals.taskCreationError(taskTitle),
         },
       );
       // eslint-disable-next-line no-console
