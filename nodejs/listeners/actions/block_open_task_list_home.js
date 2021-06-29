7ea626abcd974a2387f9aa089bcd4864e85feb80
Colm Doyle <cdoyle@slack-corp.com>
2021-06-29 12:37:11 +0100

    limit search to tasks with scheduled message IDs

--- nodejs/listeners/actions/block_open_task_list_home.js
+++ nodejs/listeners/actions/block_open_task_list_home.js
@@ -1,3 +1,4 @@
+const { Op } = require('sequelize');
 const { Task } = require('../../models');
 const { reloadAppHome } = require('../../utilities');
 
@@ -9,20 +10,28 @@ module.exports = (app) => {
     if (action.selected_options.length > 0) {
       const tasksToUpdate = action.selected_options.map((option) => option.value.split('-')[2]);
       Task.update({ status: 'CLOSED' }, { where: { id: tasksToUpdate } });
-      const tasks = await Task.findAll({ where: { id: tasksToUpdate } });
+      // Find all the tasks provided where we have a scheduled message ID
+      const tasks = await Task.findAll(
+        {
+          where: {
+            [Op.and]: [
+              { id: tasksToUpdate },
+              { scheduledMessageId: { [Op.not]: null } },
+            ],
+          },
+        },
+      );
       tasks.map(async (task) => {
-        if (task.scheduledMessageId) {
-          // If a reminder is scheduled, cancel it and remove the ID from the datastore
-          try {
-            await client.chat.deleteScheduledMessage({
-              channel: body.user.id,
-              scheduled_message_id: task.scheduledMessageId,
-            });
-            Task.update({ scheduledMessageId: null }, { where: { id: task.id } });
-          } catch (error) {
-            // eslint-disable-next-line no-console
-            console.error(error);
-          }
+        // If a reminder is scheduled, cancel it and remove the ID from the datastore
+        try {
+          await client.chat.deleteScheduledMessage({
+            channel: body.user.id,
+            scheduled_message_id: task.scheduledMessageId,
+          });
+          Task.update({ scheduledMessageId: null }, { where: { id: task.id } });
+        } catch (error) {
+          // eslint-disable-next-line no-console
+          console.error(error);
         }
       });
     }
