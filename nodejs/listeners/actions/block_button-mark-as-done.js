a2a37ec1eb14bc6b9faad7a43e37ffe65454b913
Colm Doyle <cdoyle@slack-corp.com>
2021-06-30 15:52:39 +0100

    abstract task closing logic

--- nodejs/listeners/actions/block_button-mark-as-done.js
+++ nodejs/listeners/actions/block_button-mark-as-done.js
@@ -1,6 +1,4 @@
-const { Op } = require('sequelize');
-const { Task } = require('../../models');
-const { reloadAppHome } = require('../../utilities');
+const { completeTasks, reloadAppHome } = require('../../utilities');
 
 module.exports = (app) => {
   app.action({ action_id: 'button-mark-as-done', type: 'block_actions' }, async ({
@@ -8,31 +6,7 @@ module.exports = (app) => {
   }) => {
     await ack();
     const taskID = action.value.split('-')[1];
-    Task.update({ status: 'CLOSED' }, { where: { id: taskID } });
-    // Find all the tasks provided where we have a scheduled message ID
-    const taskFromDB = await Task.findOne(
-      {
-        where: {
-          [Op.and]: [
-            { id: taskID },
-            { scheduledMessageId: { [Op.not]: null } },
-          ],
-        },
-      },
-    );
-    // If a reminder is scheduled, cancel it and remove the ID from the datastore
-    if (taskFromDB.scheduledMessageId) {
-      try {
-        await client.chat.deleteScheduledMessage({
-          channel: body.user.id,
-          scheduled_message_id: taskFromDB.scheduledMessageId,
-        });
-        Task.update({ scheduledMessageId: null }, { where: { id: taskFromDB.id } });
-      } catch (error) {
-        // eslint-disable-next-line no-console
-        console.error(error);
-      }
-    }
+    await completeTasks([taskID], body.user.id, client);
     // TODO Update the reminder message to remove the "Mark as done button"
     await reloadAppHome(client, body.user.id, body.team.id, 'completed');
   });
--- nodejs/listeners/actions/block_open_task_list_home.js
+++ nodejs/listeners/actions/block_open_task_list_home.js
@@ -1,6 +1,4 @@
-const { Op } = require('sequelize');
-const { Task } = require('../../models');
-const { reloadAppHome } = require('../../utilities');
+const { reloadAppHome, completeTasks } = require('../../utilities');
 
 module.exports = (app) => {
   app.action({ action_id: 'blockOpenTaskCheckboxClicked', type: 'block_actions' }, async ({
@@ -9,31 +7,7 @@ module.exports = (app) => {
     await ack();
     if (action.selected_options.length > 0) {
       const tasksToUpdate = action.selected_options.map((option) => option.value.split('-')[2]);
-      Task.update({ status: 'CLOSED' }, { where: { id: tasksToUpdate } });
-      // Find all the tasks provided where we have a scheduled message ID
-      const tasks = await Task.findAll(
-        {
-          where: {
-            [Op.and]: [
-              { id: tasksToUpdate },
-              { scheduledMessageId: { [Op.not]: null } },
-            ],
-          },
-        },
-      );
-      tasks.map(async (task) => {
-        // If a reminder is scheduled, cancel it and remove the ID from the datastore
-        try {
-          await client.chat.deleteScheduledMessage({
-            channel: body.user.id,
-            scheduled_message_id: task.scheduledMessageId,
-          });
-          Task.update({ scheduledMessageId: null }, { where: { id: task.id } });
-        } catch (error) {
-          // eslint-disable-next-line no-console
-          console.error(error);
-        }
-      });
+      await completeTasks(tasksToUpdate, body.user.id, client);
     }
     await reloadAppHome(client, body.user.id, body.team.id);
   });
