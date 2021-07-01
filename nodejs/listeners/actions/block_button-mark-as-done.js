1fd6ca694951160fda37c4148a9350e8518c8ccf
Colm Doyle <cdoyle@slack-corp.com>
2021-07-01 12:42:27 +0100

    clear mark as done button

--- nodejs/listeners/actions/block_button-mark-as-done.js
+++ nodejs/listeners/actions/block_button-mark-as-done.js
@@ -7,7 +7,12 @@ module.exports = (app) => {
     await ack();
     const taskID = action.value.split('-')[1];
     await completeTasks([taskID], body.user.id, client);
-    // TODO Update the reminder message to remove the "Mark as done button"
+    await client.chat.update({
+      channel: body.container.channel_id,
+      ts: body.container.message_ts,
+      text: `~${body.message.text}~`,
+      blocks: [], // Remove all the existing blocks, just leaving the text above.
+    });
     await reloadAppHome(client, body.user.id, body.team.id, 'completed');
   });
 };
