0b43d0db6d42af954951f8d7dc20fddcd16b0abb
Kian Attari <kattari@slack-corp.com>
2021-07-23 21:39:31 -0400

    message shortcut implemented

--- nodejs/listeners/shortcuts/__tests__/message-new-task.test.js
+++ nodejs/listeners/shortcuts/__tests__/message-new-task.test.js
@@ -1,9 +1,22 @@
 const { messageNewTaskCallback } = require('../message-new-task');
 const { modals } = require('../../../user-interface');
 
-it('handles the shortcut event and opens the newTask modal', async () => {
+describe('Message shortcut callback function test ', () => {
+  let errorSpy;
+
+  // Spy on the global console so we can make sure "console.error()" gets called when there is an error.
+  // We also mock it to do nothing, so that it doesn't actually write to the console. We just need to know it was called
+  // TODO: Update this when we find a better way to do errors (ideally we should just be raising an exception here and logging elsewhere).
+  beforeAll(() => {
+    errorSpy = jest.spyOn(global.console, 'error').mockImplementation(() => {});
+  });
+
+  // Restore the console.error() function to its original implementation so we don't affect other tests.
+  afterAll(() => {
+    errorSpy = global.console.error.mockRestore();
+  });
+
   const mockShortcutPayloadData = {
-    token: 'Nj2rfC2hU8mAfgaJLemZgO7H',
     callback_id: 'message_new_task',
     type: 'message_action',
     trigger_id: '13345224609.8534564800.6f8ab1f53e13d0cd15f96106292d5536',
@@ -39,18 +52,41 @@ it('handles the shortcut event and opens the newTask modal', async () => {
     },
   };
 
-  // The callback is asynchronously called so we await it here
-  await messageNewTaskCallback(mockShortcutCallbackInput);
+  const openViewsFunction = mockShortcutCallbackInput.client.views.open;
 
-  expect(mockShortcutCallbackInput.ack).toBeCalledTimes(1);
-  expect(mockShortcutCallbackInput.client.views.open).toBeCalledTimes(1);
-  expect(mockShortcutCallbackInput.client.views.open).toBeCalledWith(
-    expect.objectContaining({
-      trigger_id: mockShortcutPayloadData.trigger_id,
-      view: modals.newTask(
-        mockShortcutPayloadData.message.text,
-        mockShortcutPayloadData.user.id,
-      ),
-    }),
-  );
+  it('handles the shortcut event and opens the newTask modal', async () => {
+    // The callback is asynchronously called so we await it here
+    await messageNewTaskCallback(mockShortcutCallbackInput);
+    // jest.spyOn(global.console, 'error')
+
+    expect(mockShortcutCallbackInput.ack).toBeCalledTimes(1);
+    expect(openViewsFunction).toBeCalledTimes(1);
+    expect(openViewsFunction).toBeCalledWith(
+      expect.objectContaining({
+        trigger_id: mockShortcutPayloadData.trigger_id,
+        view: modals.newTask(
+          mockShortcutPayloadData.message.text,
+          mockShortcutPayloadData.user.id,
+        ),
+      }),
+    );
+
+    // This test isn't explicitly appropriate here since it's really testing the blockbuilder code in user-interface/modals/new-task.js
+    // which already has a test of its own. I just thought it's a good example of how you can grab the arguments of a function and test them separately.
+    const returnedBlocks = JSON.parse(
+      openViewsFunction.mock.calls[0][0].view,
+    ).blocks;
+    const titleBlock = returnedBlocks[0].element;
+    expect(titleBlock.initial_value).toBe(mockShortcutPayloadData.message.text);
+  });
+
+  it("logs an error if the modal can't be opened", async () => {
+    const errorMsg = 'Oh no! We have an error';
+    openViewsFunction.mockRejectedValueOnce(new Error(errorMsg));
+    await messageNewTaskCallback(mockShortcutCallbackInput);
+
+    expect(mockShortcutCallbackInput.ack).toBeCalledTimes(1);
+    expect(openViewsFunction).toBeCalledTimes(1);
+    expect(errorSpy).toBeCalledTimes(1);
+  });
 });
