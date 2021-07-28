7b36551d459e004aa636e930e500a45252e7f905
Kian Attari <kattari@slack-corp.com>
2021-07-27 22:46:29 -0400

    view listener skeleton

---
+++ nodejs/listeners/views/__tests__/__utils__/view-test-util-funcs.js
@@ -0,0 +1,7 @@
+/* ----------------------------------------------- Mocked API methods ----------------------------------------------- */
+
+/* -------------------- Functions for generating the inputs to the listener's callback functions. ------------------- */
+
+/* ------------------------------------- Utility functions for testing the listeners ------------------------------------ */
+
+module.exports = {};
---
+++ nodejs/listeners/views/__tests__/new-task-modal.test.js
@@ -0,0 +1,7 @@
+// const { globalNewTaskCallback } = require('../new-task-modal');
+// const {} = require('./__fixtures__/view-fixtures');
+// const {} = require('./__utils__/view-test-util-funcs');
+
+describe('View listener callback function test ', () => {
+  it('handles the view payload ', async () => {});
+});
