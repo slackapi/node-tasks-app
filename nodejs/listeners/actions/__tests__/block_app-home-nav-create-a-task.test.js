875406a9fb8b79f89a1277ded1a0bc789f1c1408
Kian Attari <kattari@slack-corp.com>
2021-07-28 23:52:26 -0400

    major restructuring

--- nodejs/listeners/actions/__tests__/block_app-home-nav-create-a-task.test.js
+++ nodejs/listeners/actions/__tests__/block_app-home-nav-create-a-task.test.js
@@ -1,6 +1,28 @@
-const {} = require('./__fixtures__/action-fixtures');
-const {} = require('./__utils__/shortcut-test-util-funcs');
+const { appHomeBlockActionPayload } = require('./__fixtures__/action-fixtures');
 
-describe('App home nav completed action callback function test ', () => {
-  it('works properly', async () => {});
+const {
+  mockActionCallbackInput,
+  testAction,
+} = require('./__utils__/action-test-util-funcs');
+const {
+  appHomeNavCreateATaskCallback,
+} = require('../block_app-home-nav-create-a-task');
+
+describe('App home nav create a task action callback function test ', () => {
+  const appHomeNavCompletedActionCallbackInput = mockActionCallbackInput(
+    appHomeBlockActionPayload,
+  );
+  it('Acknowledges the action and reloads the app home', async () => {
+    await testAction(
+      appHomeNavCreateATaskCallback(appHomeNavCompletedActionCallbackInput),
+      appHomeBlockActionPayload.user.id,
+    );
+  });
+  it('Logs an error when the the new view fails to be published', async () => {
+    await global.testErrorLog(
+      appHomeNavCreateATaskCallback(appHomeNavCompletedActionCallbackInput),
+      global.openViewMockFunc,
+      false,
+    );
+  });
 });
